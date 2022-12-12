207b6f4e2c77125da9c942686d2653485ce62f37
eungyukm <eungyukm@gmail.com>
2022-12-12 15:35:16 +0900

    upload

---
+++ Dokdo-Metaverse/Assets/Performance Tools/AdvancedRenderSystem/Example/Scripts/FPSCounter.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace NGS.AdvancedRenderSystem.Examples
+{
+    public class FPSCounter : MonoBehaviour
+    {
+        [SerializeField]
+        private Text _textField;
+
+        private float _passed;
+        private int _frames;
+
+        private void Start()
+        {
+            Application.targetFrameRate = 60;
+        }
+
+        private void Update()
+        {
+            _passed += Time.deltaTime;
+            _frames++;
+
+            if (_passed > 1)
+            {
+                if(_textField) _textField.text = _frames.ToString();
+
+                _passed = 0;
+                _frames = 0;
+            }
+        }
+    }
+}
