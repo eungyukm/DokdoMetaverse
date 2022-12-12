3804cab7c0a82a8edb21d0f823a062c1e2c2b2ce
eungyukm <eungyukm@gmail.com>
2022-12-12 09:53:31 +0900

    독도 메타버스 업로드

---
+++ Dokdo-Metaverse/Assets/1. Programmer/etc/01_Programing/02_Scripts/EventCameraDisabler.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class EventCameraDisabler : MonoBehaviour
+{
+    [SerializeField]
+    private Camera _camera;
+
+    [SerializeField]
+    private AudioListener audioListener;
+
+    private void Awake()
+    {
+        _camera = GetComponent<Camera>();
+        audioListener = GetComponent<AudioListener>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (Camera.main != null)
+        {
+            _camera.enabled = false;
+            audioListener.enabled = false;
+        }
+        else
+        {
+            _camera.enabled = true;
+            audioListener.enabled = true;
+        }
+    }
+}
