084231fa68cf1c51ec61b827a5d21b25a828e25f
eungyukm <eungyukm@gmail.com>
2022-12-12 09:56:23 +0900

    업로드

---
+++ Dokdo-Metaverse/Assets/1. Programmer/Inputs/Editor/ArowanaInputSystem_Editor.cs
@@ -0,0 +1,12 @@
+﻿using UnityEditor;
+
+[CustomEditor(typeof(ArowanaInputSystem))]
+public class ArowanaInputSystem_Editor : Editor
+{
+    ArowanaInputSystem inputSystem;
+
+    private void OnEnable()
+    {
+        inputSystem = (ArowanaInputSystem)target;
+    }
+}
